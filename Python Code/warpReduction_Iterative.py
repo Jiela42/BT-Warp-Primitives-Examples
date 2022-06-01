761ed4fd2403d04d993d086056bb95bb78e53e95
Jiela <rotkehlchen789@gmail.com>
2022-06-01 15:41:38 +0200

    tried out some things and put it into comments to not have to rewrite the code

--- Python Code/warpReduction_Iterative.py
+++ Python Code/warpReduction_Iterative.py
@@ -1,4 +1,5 @@
 
+from math import floor
 import numpy as np
 import dace
 
@@ -24,37 +25,80 @@ checkSum = np.sum(res_vec);
 print(checkSum)
 
 Size = dace.symbol('Size')
+Overshoot = dace.symbol('Overshoot')
 
 sdfg = dace.SDFG('reduction')
 
 sdfg.add_symbol('Size', stype=dace.int32)
-#sdfg.add_scalar('dSize', dtype=dace.int32)
+sdfg.add_symbol('Overshoot', stype=dace.int32)
 sdfg.add_array('A', shape=[size], dtype=dace.float64)
 sdfg.add_array('B', shape=[size], dtype=dace.float64)
 
 sdfg.add_array('Res', shape=[size], dtype=dace.float64)
 
+floorPow = 1
 
+while(floorPow < size):
+    floorPow = floorPow << 1
 
+if floorPow > size:
+    floorPow = floorPow >> 1
+
+overshoot = size - floorPow
+size = floorPow
+
+"""
 def hoistedState(state, A, B, r):
-
-    # size = state.add_access('dSize')
 
     src_A = state.add_read(A)
     src_B = state.add_read(B)
 
     dst_node = state.add_write(r)
 
-    me, mx = state.add_map('multiplyAll', dict(i='0:Size'))
-    tasklet = state.add_tasklet('mult', {'in1', 'in2'}, {'out'}, 'out = in1 * in2')
+    mme, mmx = state.add_map('multiplyAll', dict(i='0:Size+Overshoot'))
+    tMult = state.add_tasklet('mult', {'in1', 'in2'}, {'out'}, 'out = in1 * in2')
 
-    # me.add_in_connector('numElmts')
+    ame, amx = state.add_map('addOvershoot', dict(j = 'Size:Size+Overshoot'))
+    tAdd = state.add_tasklet('add_overshoot', {'in11, in22'}, {'out'}, 'out = in11 + in22')
 
-    #state.add_edge(size, None, me, 'numElmts', dace.Memlet('dSize'))
+    state.add_memlet_path(src_A, mme, tMult, dst_conn='in1', memlet=dace.Memlet(data=A, subset='i'))
+    state.add_memlet_path(src_B, mme, tMult, dst_conn='in2', memlet=dace.Memlet(data=B, subset = 'i'))
+    state.add_memlet_path(tMult, mmx, dst_node, src_conn='out', memlet=dace.Memlet(data=r, subset='i'))
 
-    state.add_memlet_path(src_A, me, tasklet, dst_conn='in1', memlet=dace.Memlet(data=A, subset='i'))
-    state.add_memlet_path(src_B, me, tasklet, dst_conn='in2', memlet=dace.Memlet(data=B, subset = 'i'))
-    state.add_memlet_path(tasklet, mx, dst_node, src_conn='out', memlet=dace.Memlet(data=r, subset='i'))
+    state.add_memlet_path(dst_node, ame, tAdd, dst_conn='in11', memlet=dace.Memlet(data=r, subset = 'Size:Size + Overshoot'))
+    state.add_memlet_path(dst_node, ame, tAdd, dst_conn='in22', memlet=dace.Memlet(data=r, subset = 'Size:Size + Overshoot'))
+    state.add_memlet_path(tMult, amx, dst_node, src_conn='out', memlet=dace.Memlet(data=r, subset ='0:Overshoot'))
+"""
+
+"""
+"""
+def hoistedState(state, A, B, r):
+
+    src_A = state.add_read(A)
+    src_B = state.add_read(B)
+
+    dst_node = state.add_write(r)
+
+    mme, mmx = state.add_map('multiplyAll', dict(i='0:Size+Overshoot'))
+    tMult = state.add_tasklet('mult', {'in1', 'in2'}, {'out'}, 'out = in1 * in2')
+
+    state.add_memlet_path(src_A, mme, tMult, dst_conn='in1', memlet=dace.Memlet(data=A, subset='i'))
+    state.add_memlet_path(src_B, mme, tMult, dst_conn='in2', memlet=dace.Memlet(data=B, subset = 'i'))
+    state.add_memlet_path(tMult, mmx, dst_node, src_conn='out', memlet=dace.Memlet(data=r, subset='i'))
+
+"""
+def reduce_overshootState(state, r):
+    node = state.add_access(r)
+
+    me, mx = state.add_map ('AddOvershoot', dict(i = 'Size:Size+Overshoot'))
+    tAdd = state.add_tasklet('addOvershoot', {'in1', 'in2'}, {'out'}, 'out = in1 + in2')
+    
+    state.add_memlet_path(node, me, tAdd, dst_conn='in1', memlet=dace.Memlet(data=r, subset= '0:Overshoot'))
+    state.add_memlet_path(node, me, tAdd, dst_conn='in2', memlet=dace.Memlet(data=r, subset= 'Size:Size+Overshoot'))
+    state.add_memlet_path(tAdd, mx, node, src_conn= 'out', memlet= dace.Memlet(data=r, subset= 'O:Overshoot'))
+
+"""
+
 
 def partitionState(state, A, res_low, res_high):
 
@@ -96,6 +140,9 @@ def report_end(state):
 state0 = sdfg.add_state('Hoisted')
 hoistedState(state0, 'A', 'B', 'Res')
 
+#state_overshoot = sdfg.add_state('Handle_Overshoot')
+#reduce_overshootState(state_overshoot, 'Res')
+
 # instead I might wanna use a fancy interstate edge that does some partitioning <3
 state_SplitArr = sdfg.add_state('SplitArr')
 partitionState(state_SplitArr, 'Res', 'A', 'B')
@@ -109,6 +156,8 @@ end_state = sdfg.add_state()
 report_end(end_state)
 
 
+# sdfg.add_edge(state0, state_overshoot, dace.InterstateEdge())
+# sdfg.add_edge(state_overshoot, guard, dace.InterstateEdge(assignments= dict(s='Size')))
 sdfg.add_edge(state0, guard, dace.InterstateEdge(assignments= dict(s='Size')))
 sdfg.add_edge(guard, state_SplitArr, dace.InterstateEdge())
 sdfg.add_edge(state_SplitArr, state_reductionStep, dace.InterstateEdge('s > 1', assignments= dict(s = 's/2')))
@@ -120,6 +169,6 @@ sdfg.simplify
 
 print(res[0])
 
-sdfg(A=a, B=b, Res=res, Size=size)
+sdfg(A=a, B=b, Res=res, Size=size, Overshoot=overshoot)
 #sdfg.validate()
 print("Ended")
