bf77d995c808736121473176434e9e89d6e739d3
Jiela <knechtd@student.ethz.ch>
2022-06-23 15:17:27 +0200

    maps on kernel

--- Python Code/warpReduction_GPU.py
+++ Python Code/warpReduction_GPU.py
@@ -3,11 +3,14 @@ import numpy as np
 import dace
 from dace.transformation.interstate import GPUTransformSDFG
 from dace.transformation.interstate import StateFusion
+from dace.transformation.dataflow import MapExpansion
+
+from dace.sdfg import nodes
 
 # Da goal: sum ai * bi
 # Specifically: put some of this into the gpu (just SOMETHING!!)
 
-size = (1 << 5) + 3
+size = (1 << 10)
 a = np.ones((size), dtype=float)
 b = np.ones((size), dtype=float)
 res = np.zeros((size), dtype=float)
@@ -48,25 +51,70 @@ size = floorPow
 
 def KernelCall(state):
 
+    tasklet_code = f"""
+    /* since every thread adds up two numbers (in the first iteration)
+    we need double the block-starting point to keep working on disjoint parts of the input */
+    int id = j + i;
+    int stepSize = Size/2;
+
+    int iterations = 1;
+    int logSize = Size;
+
+    while(logSize > 1){{
+        logSize /= 2;
+        iterations++;
+    }}
+
+    // first iteration: 
+    out[id] = (in1[id] * in2[id]) + (in1[id + stepSize] * in2[id + stepSize]);
+    stepSize /= 2; 
+
+    for(int i = 1; i < iterations; i++){{
+        if (id - 2 * (blockDim.x * blockIdx.x) < stepSize){{
+            out[id] += out[id + stepSize];
+            stepSize /= 2;
+        }}
+        __syncthreads();
+    }}
+    """
 
 
-    tasklet = state.add_tasklet(name='callingKernel',
-                                inputs={'in1', 'in2'},
-                                outputs={'out'},
-                                code=
-    ''' void* args[] = {&in1, &in2, &out, &Size};
-    cudaLaunchKernel((void*)naiveGlobalMem, dim3 (1), dim3(Size), args, 0, __dace_current_stream);''',
-                                language=dace.Language.CPP)
+    tasklet, me, mx = state.add_mapped_tasklet(
+        name='callingKernel',
+        map_ranges={'i': '0:Size/2:64', 'j': '0:64'},
+        inputs={'in1': dace.Memlet('gA[0:Size]'), 'in2': dace.Memlet('gB[0:Size]')},
+        outputs={'out': dace.Memlet('gRes[0:Size]')},
+        code=tasklet_code,
+        language=dace.dtypes.Language.CPP,
+        external_edges=True
+    )
+    me.map.schedule = dace.dtypes.ScheduleType.GPU_Device
+    
+    for n in state.nodes():
+        if isinstance(n, nodes.AccessNode):
+            if n.data == 'gA':
+                gA = n
+            elif n.data == 'gB':
+                gB = n
+            elif n.data == 'gRes':
+                gRes = n
+    # tasklet = state.add_tasklet(name='callingKernel',
+    #                             inputs={'in1', 'in2'},
+    #                             outputs={'out'},
+    #                             code=
+    # ''' void* args[] = {&in1, &in2, &out, &Size};
+    # cudaLaunchKernel((void*)naiveGlobalMem, dim3 (1), dim3(Size), args, 0, __dace_current_stream);''',
+    #                             language=dace.Language.CPP)
     A = state.add_read('A')
     B = state.add_read('B')
     Res = state.add_write('Res')
-    gA = state.add_access('gA')
-    gB = state.add_access('gB')
-    gRes = state.add_access('gRes')
+    # gA = state.add_access('gA')
+    # gB = state.add_access('gB')
+    # gRes = state.add_access('gRes')
 
-    state.add_edge(gA, None, tasklet, 'in1', dace.Memlet('gA[0:Size]'))
-    state.add_edge(gB, None, tasklet, 'in2', dace.Memlet('gB[0:Size]'))
-    state.add_edge(tasklet, 'out', gRes, None, dace.Memlet('gRes[0:Size]'))
+    # state.add_edge(gA, None, tasklet, 'in1', dace.Memlet('gA[0:Size]'))
+    # state.add_edge(gB, None, tasklet, 'in2', dace.Memlet('gB[0:Size]'))
+    # state.add_edge(tasklet, 'out', gRes, None, dace.Memlet('gRes[0:Size]'))
 
     state.add_nedge(A, gA, dace.Memlet(data='gA', subset='0:Size'))
     state.add_nedge(B, gB, dace.Memlet(data='gB', subset=' 0:Size'))
@@ -76,35 +124,42 @@ def KernelCall(state):
 callState = sdfg.add_state()
 KernelCall(callState)
 
-sdfg.append_global_code('''
-__global__ void naiveGlobalMem(double * a, double * b, double* res, int höck){
+sdfg.apply_transformations_repeated(MapExpansion)
 
-    /* since every thread adds up two numbers (in the first iteration)
-    we need double the block-starting point to keep working on disjoint parts of the input */
-    int id = threadIdx.x + 2*(blockIdx.x * blockDim.x);
-    int stepSize = höck/2;
+for n in callState.nodes():
+    if isinstance(n, nodes.MapEntry) and "j" in n.map.params:
+        n.map.schedule = dace.dtypes.ScheduleType.GPU_ThreadBlock
+        
 
-    int iterations = 1;
-    int logSize = höck;
+# sdfg.append_global_code('''
+# __global__ void naiveGlobalMem(double * a, double * b, double* res, int höck){
 
-    while(logSize > 1){
-        logSize /= 2;
-        iterations++;
-    }
+    # /* since every thread adds up two numbers (in the first iteration)
+    # we need double the block-starting point to keep working on disjoint parts of the input */
+    # int id = threadIdx.x + 2*(blockIdx.x * blockDim.x);
+    # int stepSize = höck/2;
 
-    // first iteration: 
-    res[id] = (a[id] * b[id]) + (a[id + stepSize] * b[id + stepSize]);
-    stepSize /= 2; 
+    # int iterations = 1;
+    # int logSize = höck;
 
-    for(int i = 1; i < iterations; i++){
-        if (id - 2 * (blockDim.x * blockIdx.x) < stepSize){
-            res[id] += res[id + stepSize];
-            stepSize /= 2;
-        }
-        __syncthreads();
-    }
-}
-''')
+    # while(logSize > 1){
+    #     logSize /= 2;
+    #     iterations++;
+    # }
+
+    # // first iteration: 
+    # res[id] = (a[id] * b[id]) + (a[id + stepSize] * b[id + stepSize]);
+    # stepSize /= 2; 
+
+    # for(int i = 1; i < iterations; i++){
+    #     if (id - 2 * (blockDim.x * blockIdx.x) < stepSize){
+    #         res[id] += res[id + stepSize];
+    #         stepSize /= 2;
+    #     }
+    #     __syncthreads();
+    # }
+# }
+# ''')
 
 
 sdfg(A=a, B=b, Res=res, Size=size, Overshoot=overshoot)
