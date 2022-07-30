c264bd33f6217e6bf1242d5741653b760f90977e
Jiela <rotkehlchen789@gmail.com>
2022-07-30 14:11:23 +0200

    Changed file to only call reduction library node

--- Python Code/warp_Reduction_GPU_Nested_LibNode.py
+++ Python Code/warp_Reduction_GPU_Nested_LibNode.py
@@ -8,8 +8,7 @@ from dace.sdfg import nodes
 from dace.transformation.auto import auto_optimize as auto
 from dace.transformation.dataflow import MapExpansion
 from sympy import true
-
-import WarpReductionExpansion
+from dace.libraries.standard.nodes import Reduce
 
 # I keep hardcoding the maxNumber of threads, we might wanna add a symbol instead
 
@@ -65,136 +64,24 @@ sdfg2.add_symbol('GridDim', stype= dace.int32)
 sdfg2.add_symbol('BlockDim', stype= dace.int32)
 sdfg2.add_symbol('WarpSize', stype= dace.int32)
 
-def Init(state, m):
-    dst_node = state.add_write(m)
-    init_t = state.add_tasklet('init_out', {}, {'__out'}, '__out = 0')
-    
-    state.add_edge (init_t, '__out', dst_node, None, dace.Memlet(data=m))
-    
 
-def GridStrideLoop(state, i1, i2, mS):
+somestate= sdfg2.add_state()
 
-    init_tasklet = state.add_tasklet('init_sum', {}, {'__out'}, '__out = 0')
-    sum_node = state.add_access(mS)
-    state.add_edge(init_tasklet, '__out', sum_node, None, dace.Memlet.from_array(mS, state.parent.arrays[mS]))
-    
-    src_A = state.add_read(i1)
-    src_B = state.add_read(i2)
-    
-    dst_node = state.add_access(mS)
-    
-    me,mx = state.add_map('gridSized_strides_map', dict(tId = 'i*BlockDim+j:N:MaxTs'))
-    tasklet = state.add_tasklet('mult', {'in1', 'in2', '__in3'}, {'out'},  'out = in1 * in2 + __in3')
-    
-    state.add_memlet_path(src_A, me, tasklet, dst_conn='in1', memlet=dace.Memlet(data=i1, subset='tId'))
-    state.add_memlet_path(src_B, me, tasklet, dst_conn='in2', memlet=dace.Memlet(data=i2, subset = 'tId'))
-    state.add_memlet_path(sum_node, me, tasklet, dst_conn='__in3', memlet=dace.Memlet.from_array(mS, state.parent.arrays[mS]))
-    state.add_memlet_path(tasklet, mx, dst_node, src_conn='out', memlet=dace.Memlet(data=mS, subset='0'))
-    
-def WriteBackState(state, mS, r):
-    
-    src_node = state.add_read(mS)
-    dst_node = state.add_write(r)
-    
-    state.add_nedge(src_node, dst_node, dace.Memlet(f"{r}", wcr="lambda x, y: x + y"))      
+aA = somestate.add_access('A')
+aret = somestate.add_access('__return')
 
-##################################
-# Create the GPU scheduleing state
-
-gpuCallState = sdfg2.add_state()
-
-me, mx = gpuCallState.add_map('GPU_map', {'i': '0:min(int_ceil(N, BlockDim), GridDim)', 'j': '0:BlockDim'})
-me.map.schedule = dace.dtypes.ScheduleType.GPU_Device
-
-##################################
+red = Reduce(wcr = 'lambda a,b: a + b',
+            identity = 0)
 
 
-##################################
-# make the nested SDFG happen
-
-gpu_sdfg = dace.SDFG('GPU_SDFG')
-
-gpu_sdfg.add_array('sA', shape= [N], dtype=dace.float64, storage=dace.StorageType.GPU_Global)
-gpu_sdfg.add_array('sB', shape= [N], dtype= dace.float64, storage=dace.StorageType.GPU_Global)
-gpu_sdfg.add_array('sRes', shape=[1], dtype=dace.float64, storage=StorageType.GPU_Global)
-gpu_sdfg.add_scalar('mySum', dtype=dace.float64, storage=StorageType.Register, transient=True)
-
-# create inner sdfg
-entry_state = gpu_sdfg.add_state('Entry_node')
-
-init_sRes = gpu_sdfg.add_state('Init_sRes')
-Init(init_sRes, 'sRes')
-
-m_state= gpu_sdfg.add_state('Mult')
-GridStrideLoop(m_state, 'sA', 'sB', 'mySum')
-
-# The following commented out code is supposed to be turned into a library node
-
-# gpu_sdfg.add_edge(entry_state, init_sRes, dace.InterstateEdge('j+i == 0'))
-# gpu_sdfg.add_edge(entry_state, m_state, dace.InterstateEdge('j+i != 0'))
-
-# gpu_sdfg.add_edge(init_sRes, m_state, dace.InterstateEdge())
-
-# wwr_state= gpu_sdfg.add_state('WarpWise_Reduction')
-
-# mSum = wwr_state.add_access('mySum')
-# wtasklet = wwr_state.add_tasklet('warpwise_Reduction',
-#                                  {}, {'__out'},
-#                                  f"__out = __shfl_down_sync(0xFFFFFFFF, {mSum.data}, offset);",
-#                                  dace.Language.CPP)
-# wwr_state.add_edge(wtasklet, '__out', mSum, None, dace.Memlet(f"{mSum.data}", wcr="lambda x, y: x + y"))
-
-# _, _, after_state = gpu_sdfg.add_loop(m_state, wwr_state, None, 'offset', 'WarpSize / 2', 'offset > 0', 'offset / 2')
-
-# write_back_state = gpu_sdfg.add_state('Write_Back')
-# WriteBackState(write_back_state, 'mySum', 'sRes')
-
-
-# gpu_sdfg.add_edge(after_state, write_back_state, dace.InterstateEdge('j % WarpSize == 0'))
-# gpu_sdfg.add_edge(after_state, random_end_state, dace.InterstateEdge('j % WarpSize != 0'))
-# gpu_sdfg.add_edge(write_back_state, random_end_state, dace.InterstateEdge())
-
-in_between = gpu_sdfg.add_state('Node_to_Expand')
-
-random_end_state = gpu_sdfg.add_state('RandomEndState')
-
-gpu_sdfg.add_edge(in_between, random_end_state, dace.InterstateEdge())
-
-WarpReductionExpansion.apply_to(
-    node=in_between,
-    parent_state= None,
-    parent_sdfg= gpu_sdfg,
-    local_sum = 'mySum',
-    local_result ='sRes')
-
-
-##################################
-
-
-##################################
-# Make the dataflow between the states happen
-da_whole_SDFG = gpuCallState.add_nested_sdfg(gpu_sdfg, sdfg2, {'sA', 'sB'}, {'sRes'})
-
-Ain = gpuCallState.add_read('A')
-Bin = gpuCallState.add_read('B')
-ROut = gpuCallState.add_write('__return')
-
-gpuCallState.add_memlet_path(Ain, me, da_whole_SDFG, memlet=dace.Memlet(data='A', subset='0: (min(int_ceil(N, BlockDim), GridDim) * BlockDim)'), dst_conn='sA')
-gpuCallState.add_memlet_path(Bin, me, da_whole_SDFG, memlet=dace.Memlet(data='B', subset='0: (min(int_ceil(N, BlockDim), GridDim) * BlockDim)'), dst_conn='sB')
-gpuCallState.add_memlet_path(da_whole_SDFG, mx, ROut, memlet=dace.Memlet(data='__return', subset='0'), src_conn='sRes')
-
-
-sdfg2.apply_transformations_repeated(MapExpansion)
-
-for n in gpuCallState.nodes():
-    if isinstance(n, nodes.MapEntry) and "j" in n.map.params:
-        n.map.schedule = dace.dtypes.ScheduleType.GPU_ThreadBlock
+somestate.add_edge(aA, None, red, None, memlet= dace.Memlet.from_array('A', somestate.parent.arrays['A']))
+somestate.add_edge(red, None, aret, None, dace.Memlet.from_array('__return', somestate.parent.arrays['__return']))
         
 
 
 res2 = sdfg2(A=gA, B=gB, __return=r, N=sz, MaxTs = blockDim * gridDim, BlockDim = blockDim, GridDim = gridDim, WarpSize= 32)
 
-print("Custom Sub SDFG")
+print("Reduce_Library_Node")
 print(res2)
 
 # assert(np.allclose(res, res2))
